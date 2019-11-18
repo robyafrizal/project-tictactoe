302652e39d8ed9803f4d42927e3e7cd31cfd4019
robyafrizal <robyafrizal86@gmail.com>
2019-11-18 12:27:38 +0700

    Add reactstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  text-align: center;
+  padding: 0.5em
+  background: ${props => (props.dark ? "#bb1e1e" : "#000")}
+  margin-top: 50px
+`;
+const Section = styled(Wrapper)`
+  color: yellow;
+`;
+
+const Header = styled.h3`
+  background: #000
+  color: #fff
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Wrapper dark>
+        <br />
+        <Header>Play With Heart</Header>
+      </Wrapper>
+      <Section>Love Games</Section>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
-// import App from "./App";
+import App from "./App";
+import NavBar from "./components/NavBar";
 import Game from "./components/game";
 import Button from "./components/button";
 import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<NavBar />, document.getElementById("navbar"));
 ReactDOM.render(<Game />, document.getElementById("root"));
 ReactDOM.render(<Button />, document.getElementById("other"));
+ReactDOM.render(<App />, document.getElementById("text"));
 
 serviceWorker.unregister();
